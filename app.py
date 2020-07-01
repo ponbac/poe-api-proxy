e0b100b0c18736f758aa4e8114735465cebfa325
ponbac <ponbac@student.chalmers.se>
2020-07-02 01:12:22 +0200

    tried to set cookie

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
+import requests
 from requests import get
+from requests import cookies
 
 app = Flask(__name__)
 CORS(app)
@@ -14,7 +16,15 @@ def index():
 @app.route('/get/', defaults={'path': ''})
 @app.route('/get/<path:path>')
 def proxy(path):
-    return get(f'{path}', headers={'cookie': 'POESESSID=5fac1b643f65f2591d27a0ab8a2bd345'}).content
+    s = requests.Session()
+    s.get(f'{path}')
+
+    cookie_obj = requests.cookies.create_cookie(domain='pathofexile.com', name='POESESSID', value='5fac1b643f65f2591d27a0ab8a2bd345')
+    s.cookies.set_cookie(cookie_obj)
+
+    print(s.cookies)
+
+    return s.get(f'{path}', cookies=s.cookies).content
 
 
 if __name__ == '__main__':
