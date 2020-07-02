5e2d6f0958f5c8cc9b923ebf8b44bf5f8f697d7e
ponbac <ponbac@student.chalmers.se>
2020-07-02 07:22:09 +0000

    working now

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask_cors import CORS
 import requests
 from requests import get
@@ -18,12 +19,17 @@ def proxy(path):
     s = requests.Session()
     s.get(f'{path}')
 
-    cookie_obj = requests.cookies.create_cookie(domain='pathofexile.com', name='POESESSID', value='xxx')
-    s.cookies.set_cookie(cookie_obj)
+    s.cookies.set('POESESSID', None)
+    s.cookies.set('POESESSID', "xxx")
+
+    # cookie_obj = requests.cookies.create_cookie(domain='pathofexile.com', name='POESESSID', value='xxx')
+    # s.cookies.set_cookie(cookie_obj)
 
     print(s.cookies)
+    print(f'{path}')
+    print(request.args)
 
-    return s.get(f'{path}', cookies=s.cookies).content
+    return s.get(f'{path}', cookies=s.cookies, params=request.args).content
 
 
 if __name__ == '__main__':
